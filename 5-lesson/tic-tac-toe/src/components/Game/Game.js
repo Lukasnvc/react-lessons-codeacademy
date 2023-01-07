828fad239af3272c4c75890c57970c95314e9618
Lukasnvc <lukasnvc@gmail.com>
2023-01-07 19:16:28 +0200

    tic-tac-toe done

---
+++ 5-lesson/tic-tac-toe/src/components/Game/Game.js
@@ -0,0 +1,48 @@
+import Board from "../Board/Board";
+import "./Game.css";
+import { useState } from "react";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button className="btn" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ 5-lesson/tic-tac-toe/src/components/Square/Square.js
@@ -0,0 +1,11 @@
+import "./Square.css";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
