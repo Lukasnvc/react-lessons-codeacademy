b4ff16f856229f495a9c6e6ae771cd48988ff530
Lukasnvc <lukasnvc@gmail.com>
2023-01-09 21:59:33 +0200

    +

--- 6-lesson/6-lesson/src/App.js
+++ 6-lesson/6-lesson/src/App.js
@@ -1,36 +1,36 @@
 import "./App.css";
-import Button from "./components/Button/Button";
-import { useState } from "react";
-import Greeting from "./components/Greeting/Greeting";
-import Hero from "./components/Hero/Hero";
-import Cards from "./components/Cards/Cards";
-import MainText from "./components/MainText/MainText";
+// import Button from "./components/Button/Button";
+// import { useState } from "react";
+// import Greeting from "./components/Greeting/Greeting";
+// import Hero from "./components/Hero/Hero";
+// import Cards from "./components/Cards/Cards";
+// import MainText from "./components/MainText/MainText";
 import MenuList from "./components/MenuList/MenuList";
 import Avatar from "./components/Avatar/Avatar";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [count, setCount] = useState(0);
-  const handleAddCount = () => {
-    setCount(count + 1);
-  };
-  const fruits = ["banana", "apple", "pear"];
+  // const [count, setCount] = useState(0);
+  // const handleAddCount = () => {
+  //   setCount(count + 1);
+  // };
+  // const fruits = ["banana", "apple", "pear"];
 
-  const buttons = [
-    {
-      variant: "text",
-      text: "help me get random number",
-    },
-    {
-      variant: "outlined",
-      text: "I am an outcast",
-    },
-    {
-      variant: "contained",
-      text: "contained button",
-    },
-  ];
+  // const buttons = [
+  //   {
+  //     variant: "text",
+  //     text: "help me get random number",
+  //   },
+  //   {
+  //     variant: "outlined",
+  //     text: "I am an outcast",
+  //   },
+  //   {
+  //     variant: "contained",
+  //     text: "contained button",
+  //   },
+  // ];
 
   return (
     <div className="App">
